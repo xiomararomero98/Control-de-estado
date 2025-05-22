ac0bfe4df726a4baba24a8517febe0b765768d55
xiomararomero98 <xio.romero@duocuc.cl>
2025-05-22 18:52:37 -0400

    Control de estado

---
+++ Control-de-Estado/src/main/java/com/example/Control_de_Estado/ControlDeEstadoApplication.java
@@ -0,0 +1,13 @@
+package com.example.Control_de_Estado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlDeEstadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlDeEstadoApplication.class, args);
+	}
+
+}
---
+++ Control-de-Estado/src/test/java/com/example/Control_de_Estado/ControlDeEstadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Control_de_Estado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlDeEstadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
